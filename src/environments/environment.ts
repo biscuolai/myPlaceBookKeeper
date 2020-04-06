7c5698ca4e48c28cd3670f0394ee1397572a97b8
biscuolai <ilson_biscuola@yahoo.com>
2020-04-06 10:07:19 +0800

    firebase deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,10 +8,12 @@ export const environment = {
   firebaseConfig : {
     apiKey: "AIzaSyAiekX6GrXTfpvJ93ZK3eGriUVsQpmdsQQ",
     authDomain: "myplanbookkeeper.firebaseapp.com",
-    databaseURL: "https://myPlanBookKeeper.firebaseio.com",
+    databaseURL: "https://myplanbookkeeper.firebaseio.com",
     projectId: "myplanbookkeeper",
     storageBucket: "myplanbookkeeper.appspot.com",
-    messagingSenderId: "406863638795"
+    messagingSenderId: "406863638795",
+    appId: "1:406863638795:web:b217c4de98643c33d3720e",
+    measurementId: "G-QJFJH8PQ6W"
   }
 };
 
