090c13526f63f083e08d3d1bc261b825452b721f
biscuolai <ilson_biscuola@yahoo.com>
2020-04-14 17:58:42 +0800

    styling forgot pass and sign in

--- src/app/auth/forgot-password/forgot-password.component.ts
+++ src/app/auth/forgot-password/forgot-password.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from "./../services/auth.service";
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password',
@@ -9,11 +11,25 @@ import { AuthService } from "./../services/auth.service";
 
 export class ForgotPasswordComponent implements OnInit {
 
+  form: FormGroup;
+
   constructor(
-    public authService: AuthService
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router
   ) { }
 
   ngOnInit() {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  recoverPassword() {
+    let email = this.form.get('email').value;
+    if (email){
+      this.authService.ForgotPassword(email);
+    }
   }
 
 }
